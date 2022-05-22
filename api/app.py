32be2bcb7243cad5b2f059a5c774be0d55234191
Aleksei Kaplin <aleksei.kaplin.2021@gmail.com>
2022-05-22 05:13:19 +0200

    Metrics exposed

--- api/app.py
+++ api/app.py
@@ -1,9 +1,14 @@
 # flask_web/app.py
 
 import os
-from flask import Flask
+from flask import Flask, request, jsonify
 from redis import Redis
+from prometheus_flask_exporter import PrometheusMetrics
+
+from config import Pool, Config
+
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
 
 # Connect to redis
 REDIS_HOST = os.getenv('REDIS_HOST')
@@ -12,8 +17,32 @@ REDIS_PASSWORD = os.getenv('REDIS_PASSWORD')
 redis = Redis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
 
 @app.route('/')
-def hello_world():
+def handle_main():
     return 'Good Morning, Vietnam!'
+
+
+@app.route('/bridge')
+def handle_bridge():
+    result = ["123"]
+    #TODO
+    return jsonify(result)
+
+
+@app.route('/report', methods=['POST'])
+def handle_report():
+    content = request.get_json(silent=True)
+    state = bool(content.get('state', False))
+    bridge = content.get('bridge', '')
+
+    if len(bridge) == 0:
+        return 'Failed', 400
+
+    if state:
+        #TODO
+        return 'Promoted'
+    else:
+        #TODO
+        return 'Punished'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3000)
---
+++ api/config.py
@@ -0,0 +1,11 @@
+class Pool:
+    
+    def conditions():
+        return []
+
+    def bridges():
+        return []
+
+class Config:
+    def pool(name: str) -> Pool:
+        return None
